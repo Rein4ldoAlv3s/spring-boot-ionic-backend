d50230bd1845769128f79d166df64b934617f227
Rein4ldoAlv3s <naldos15@hotmail.com>
2022-02-09 23:57:55 -0300

    pedido pagamento

--- src/main/java/com/reinaldo/cursomcdois/domain/Pagamento.java
+++ src/main/java/com/reinaldo/cursomcdois/domain/Pagamento.java
@@ -1,18 +1,28 @@
 package com.reinaldo.cursomcdois.domain;
 
 import java.io.Serializable;
-import java.util.Date;
 import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 
 import com.reinaldo.cursomcdois.domain.enums.EstadoPagamento;
 
+@Entity
 public class Pagamento implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
 	private Integer id;
 	private EstadoPagamento estado;
 	
+	@OneToOne
+	@JoinColumn(name="pedido_id")
+	@MapsId
 	private Pedido pedido;
 	
 	public Pagamento() {
--- src/main/java/com/reinaldo/cursomcdois/domain/Pedido.java
+++ src/main/java/com/reinaldo/cursomcdois/domain/Pedido.java
@@ -4,15 +4,26 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 import com.reinaldo.cursomcdois.domain.enums.EstadoPagamento;
 
+@Entity
 public class Pedido implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private Date instante;
 	
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
 	
 	private Cliente cliente;
