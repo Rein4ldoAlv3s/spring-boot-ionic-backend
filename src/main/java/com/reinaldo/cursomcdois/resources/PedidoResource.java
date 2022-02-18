43dbe0ef3c81036833c360f2eba60293ce798c4f
Rein4ldoAlv3s <naldos15@hotmail.com>
2022-02-18 01:09:19 -0300

    pedidos funfa

---
+++ src/main/java/com/reinaldo/cursomcdois/resources/PedidoResource.java
@@ -0,0 +1,28 @@
+package com.reinaldo.cursomcdois.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reinaldo.cursomcdois.domain.Pedido;
+import com.reinaldo.cursomcdois.services.PedidoService;
+
+@RestController
+@RequestMapping(value = "/pedidos")
+public class PedidoResource {
+	
+	@Autowired
+	private PedidoService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Pedido obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
