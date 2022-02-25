1d55245aebaae70357463368c44b176d0af5213f
Rein4ldoAlv3s <naldos15@hotmail.com>
2022-02-24 23:13:43 -0300

    insercao de categorias funciona

--- src/main/java/com/reinaldo/cursomcdois/resources/CategoriaResource.java
+++ src/main/java/com/reinaldo/cursomcdois/resources/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.reinaldo.cursomcdois.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,8 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.reinaldo.cursomcdois.domain.Categoria;
 import com.reinaldo.cursomcdois.services.CategoriaService;
@@ -20,9 +25,18 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
-	@GetMapping(value = "/{id}")
+	@RequestMapping(value="{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+		
 	}
 }
