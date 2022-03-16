05b3ba0051939eed4096a8036fd6b31092889f23
Rein4ldoAlv3s <naldos15@hotmail.com>
2022-03-16 17:03:31 -0300

    validacao email

--- src/main/java/com/reinaldo/cursomcdois/repositories/ClienteRepository.java
+++ src/main/java/com/reinaldo/cursomcdois/repositories/ClienteRepository.java
@@ -2,10 +2,13 @@ package com.reinaldo.cursomcdois.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.reinaldo.cursomcdois.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/reinaldo/cursomcdois/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/reinaldo/cursomcdois/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.reinaldo.cursomcdois.domain.Cliente;
 import com.reinaldo.cursomcdois.domain.enums.TipoCliente;
 import com.reinaldo.cursomcdois.dto.ClienteNewDto;
+import com.reinaldo.cursomcdois.repositories.ClienteRepository;
 import com.reinaldo.cursomcdois.resources.exception.FieldMessage;
 import com.reinaldo.cursomcdois.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDto> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +37,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj()) ) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
 		
 		// inclua os testes aqui, inserindo erros na lista
 		for (FieldMessage e : list) {
