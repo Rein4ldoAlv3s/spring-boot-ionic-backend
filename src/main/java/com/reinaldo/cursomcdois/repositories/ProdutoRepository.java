844eec3cdad2719ff2b848397cdef304f4f41616
Rein4ldoAlv3s <naldos15@hotmail.com>
2022-03-17 18:01:11 -0300

    cat service

--- src/main/java/com/reinaldo/cursomcdois/repositories/ProdutoRepository.java
+++ src/main/java/com/reinaldo/cursomcdois/repositories/ProdutoRepository.java
@@ -1,11 +1,16 @@
 package com.reinaldo.cursomcdois.repositories;
 
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.reinaldo.cursomcdois.domain.Categoria;
 import com.reinaldo.cursomcdois.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
-	
+	Page<Produto> search(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
---
+++ src/main/java/com/reinaldo/cursomcdois/services/ProdutoService.java
@@ -0,0 +1,37 @@
+package com.reinaldo.cursomcdois.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.reinaldo.cursomcdois.domain.Categoria;
+import com.reinaldo.cursomcdois.domain.Produto;
+import com.reinaldo.cursomcdois.repositories.CategoriaRepository;
+import com.reinaldo.cursomcdois.repositories.ProdutoRepository;
+import com.reinaldo.cursomcdois.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository repo;
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.search(nome, categorias, pageRequest);
+	}
+}
