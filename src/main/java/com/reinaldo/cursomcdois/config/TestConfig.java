5da6153d7b7af77341198b10c5a5d87198f324d1
Reinaldo <naldos15@hotmail.com>
2022-03-28 19:44:27 -0300

    profile de teste

---
+++ src/main/java/com/reinaldo/cursomcdois/config/TestConfig.java
@@ -0,0 +1,24 @@
+package com.reinaldo.cursomcdois.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.reinaldo.cursomcdois.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDabase();
+		return true;
+	}
+}
